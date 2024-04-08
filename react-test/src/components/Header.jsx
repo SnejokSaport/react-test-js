45056f8e5becfba3138d4e2c62b5a522a6e59783
Snejoksaport <Hydramist2020@mail.ru>
2024-04-08 22:05:29 +0500

    first commit

---
+++ react-test/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://cdn.dribbble.com/users/622335/screenshots/4340826/800.jpg" alt="" />
+        </header>
+    )
+}
+
+export default Header;
---
+++ react-test/src/components/Profile.jsx
@@ -0,0 +1,31 @@
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://get.wallhere.com/photo/1583x845-px-Warcraft-World-of-Warcraft-World-of-Warcraft-Wrath-of-the-Lich-King-683468.jpg" alt="" />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
+
+
+
+
+
+
